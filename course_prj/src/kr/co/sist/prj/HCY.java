b5e90f8a33138a7f9048ee38598f3ba86c3975f4
user <user@DESKTOP-COJ9858>
2023-08-22 14:58:41 +0900

    혹쉬 우산좀 빌려주실분 구해여@@@@@@@@@@@@@@@@@@@@@@@@@

--- course_prj/src/kr/co/sist/prj/HCY.java
+++ course_prj/src/kr/co/sist/prj/HCY.java
@@ -4,7 +4,18 @@ public class HCY {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("집에 가고싶은 1인");
+		System.out.println("혹시 우산빌려주실분??");
+		System.out.println("혹시 우산빌려주실분??");
+		System.out.println("혹시 우산빌려주실분??");
+		System.out.println("혹시 우산빌려주실분??");
+		System.out.println("혹시 우산빌려주실분??");
+		System.out.println("혹시 우산빌려주실분??");
+		System.out.println("혹시 우산빌려주실분??");
+		System.out.println("혹시 우산빌려주실분??");
+		System.out.println("혹시 우산빌려주실분??");
+		System.out.println("혹시 우산빌려주실분??");
+		System.out.println("혹시 우산빌려주실분??");
+		System.out.println("혹시 우산빌려주실분??");
 
 	}
 
